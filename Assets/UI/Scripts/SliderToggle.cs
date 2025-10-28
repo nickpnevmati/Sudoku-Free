c6d1b7d43c3c2a164ceece82c956ca2bf31bc2fa
Nick Pnevmatikatos <nickpnevmati@gmail.com>
2025-10-28 12:44:23 +0200

    No idea what changes I made

---
+++ Assets/UI/Scripts/SliderToggle.cs
@@ -0,0 +1,63 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Toggle))]
+public class SliderToggle : MonoBehaviour
+{
+    [SerializeField] string labelText;
+    [SerializeField] TMP_Text label;
+
+    [SerializeField] Image slider;
+    [SerializeField] Image sliderBackground;
+
+    [SerializeField] RectTransform sliderOffPos, sliderOnPos;
+    [SerializeField] float transitionTime;
+
+    private Toggle toggle;
+
+    void Awake()
+    {
+        label.text = labelText;
+
+        toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener(TriggerToggleAnimation);
+
+        slider.transform.position = toggle.isOn ? sliderOnPos.position : sliderOffPos.position;
+    }
+
+    private void TriggerToggleAnimation(bool value)
+    {
+        StopAllCoroutines();
+        StartCoroutine(ToggleAnimation(value));
+    }
+
+    private IEnumerator ToggleAnimation(bool value)
+    {
+        RectTransform origin = value ? sliderOffPos : sliderOnPos;
+        Vector3 originAnchored = origin.position;
+
+        RectTransform target = value ? sliderOnPos : sliderOffPos;
+        Vector3 targetAnchored = target.position;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < transitionTime)
+        {
+            float percent = elapsedTime / transitionTime;
+            Vector3 currentOffset = (targetAnchored - originAnchored) * percent;
+            elapsedTime += Time.deltaTime;
+
+            slider.transform.position = origin.position + currentOffset;
+
+            yield return null;
+        }
+
+        slider.transform.position = target.position;
+    }
+
+    private void OnValidate()
+    {
+        label.text = labelText;
+    }
+}
